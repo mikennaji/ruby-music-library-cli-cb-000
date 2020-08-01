c715fed49d536fef52a91d0ce8fd77f95d606bd1
Michael Nnaji <mnnaji3@uwo.ca>
2020-08-01 23:31:07 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist 
+
+attr_accessor :name
+
+
+def initialize(name)
+  @name = name
+  save
+
+end
+
+def save
+    @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def self.destroy_all
+  @@all =[]
+end
+
+def self.create(name)
+  song = Song.new(name)
+  song
+end
+
+
+end
