1bed651bbac56f706f0707f8144106dbe481a8b1
Michael Nnaji <mnnaji3@uwo.ca>
2020-08-01 23:25:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,25 @@ class Song
 
 attr_accessor :name
 
+@@all = []
+
 def initialize(name)
   @name = name
+  save
+
 end
 
+def save 
+    @@all << self
+end 
+
+def self.all 
+  @all 
+end
+
+def self.destroy_all
+  @@all =[]
+end
 
 
 
