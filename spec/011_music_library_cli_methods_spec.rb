4bfa221b3198e76328ace0ab3c234c0cafd445ad
Michael Nnaji <mnnaji3@uwo.ca>
2020-08-02 22:11:47 +0000

    Automatically backed up by Learn

--- spec/011_music_library_cli_methods_spec.rb
+++ spec/011_music_library_cli_methods_spec.rb
@@ -91,7 +91,7 @@ describe "MusicLibraryController - CLI Methods" do
       expect($stdout).to receive(:puts).with("Please enter the name of an artist:")
       expect($stdout).to receive(:puts).with("1. Green Aisles - country")
       expect($stdout).to receive(:puts).with("2. It's Real - hi-ho")
-      expect($stdout).to receive(:puts).with("3. Wonder Years - dream pop")
+      expect($stdout).to receive(:puts).with("3. Wonder Years - drea o")
 
       music_library_controller.list_songs_by_artist
     end
