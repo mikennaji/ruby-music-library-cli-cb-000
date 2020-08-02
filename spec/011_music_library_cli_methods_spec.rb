e4c46426324a00dd7b12ef8344cd936650dfb36b
Michael Nnaji <mnnaji3@uwo.ca>
2020-08-02 20:49:07 +0000

    Automatically backed up by Learn

--- spec/011_music_library_cli_methods_spec.rb
+++ spec/011_music_library_cli_methods_spec.rb
@@ -39,8 +39,8 @@ describe "MusicLibraryController - CLI Methods" do
       Artist.create("ZZ Top")
 
       expect($stdout).to receive(:puts).with("1. Alpha 9")
-      expect($stdout).to receive(:puts).with("2. Bob Dylan")
-      expect($stdout).to receive(:puts).with("3. Cass McCombs")
+      expect($stdout).to receive(:puts).with("2. Cass McCombs")
+      expect($stdout).to receive(:puts).with("3. Bob Dylan")
       expect($stdout).to receive(:puts).with("4. ZZ Top")
 
       other_music_library_controller.list_artists
