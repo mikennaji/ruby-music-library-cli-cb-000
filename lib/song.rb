a06b438d5c986f68df44c7aa6076a2060a241d88
Michael Nnaji <mnnaji3@uwo.ca>
2020-08-02 02:43:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,10 +35,10 @@ def self.find_by_name(name)
 end
 
 def self.find_or_create_by_name(name)
-  if self.find_by_name(name)===false
-    self.create(name)
+  if self.find_by_name(name)
+      self.find_by_name(name)
   else
-    self.find_by_name(name)
+    self.create(name)
   end
 
 end
