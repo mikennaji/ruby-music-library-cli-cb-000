ca45608d1f664c04615c99064527de4170773d3c
Michael Nnaji <mnnaji3@uwo.ca>
2020-08-02 16:32:17 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -4,7 +4,7 @@ class MusicLibraryController
 
 def initialize(path="./db/mp3s")
   @path= path
-  new_music = MusicImporter.new(self.path)
+  new_music = MusicImporter.new(@path)
   new_music.import
 end
 
