071ba05cb4711196f82d5c68b9c76d06fdf9d7f9
Michael Nnaji <mnnaji3@uwo.ca>
2020-08-02 22:29:47 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -4,5 +4,14 @@ module Concerns
       all.detect{|song|song.name===name}
 
     end
+    def self.find_or_create_by_name(name)
+      if find_by_name(name)
+          find_by_name(name)
+      else
+        create(name)
+      end
+
+    end
+
   end
 end
