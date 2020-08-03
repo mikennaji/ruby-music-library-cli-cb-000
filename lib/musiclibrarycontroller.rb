2e136f4e5f877e5d3294afc9237149da09d9a8ee
Michael Nnaji <mnnaji3@uwo.ca>
2020-08-03 04:34:22 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -12,16 +12,6 @@ def initialize(path='./db/mp3s')
 
 def call
   response = nil
-
-  puts "Welcome to your music library!"
-  puts "To list all of your songs, enter 'list songs'."
-  puts "To list all of the artists in your library, enter 'list artists'."
-  puts "To list all of the genres in your library, enter 'list genres'."
-  puts "To list all of the songs by a particular artist, enter 'list artist'."
-  puts "To list all of the songs of a particular genre, enter 'list genre'."
-  puts "To play a song, enter 'play song'."
-  puts "To quit, type 'exit'."
-  puts "What would you like to do?"
   while response != "exit"
     response = gets.strip
     puts "Welcome to your music library!"
@@ -73,6 +63,7 @@ def list_artists
  end
 
 
+
    def list_genres
 
       Song.all.sort{|a,b|a.name<=> b.name}.each_with_index do |value, index|
@@ -96,6 +87,24 @@ def list_artists
       puts "#{index+1}. #{value.artist.name} - #{value.name}"
     end
   end
+
+
+
+  def play_song
+    puts "Which song number would you like to play?"
+    input = gets.strip.to_i
+
+    if input >= 1 && input <= Song.all.count
+
+      array = Song.all.sort{|a,b| a.name <=> b.name}
+      s = array[(input - 1)]
+     puts "Playing #{s.name} by #{s.artist.name}"
+   end
+
+  end
+
+
+
 
 
 
